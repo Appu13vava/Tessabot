ade7581fbf5ce36c043f811287e376193c409eed
Appu13vava <94307842+Appu13vava@users.noreply.github.com>
2025-05-18 01:58:51 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,8 +4,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 @Client.on_message(filters.command("start"))
 async def start(client, message):
     # Send a sticker first
-    await message.reply_sticker("CAACAgUAAxkBAAEBVfFlnTD9i4c5DRM8K6MQN2aFFoyZuAACAwEAAvcCyFYybAIKq8ZECzQE")
-
+    
     # Send a welcome message with inline buttons
     await message.reply_text(
         text=f"Hello **{message.from_user.first_name}**!\nI'm alive and ready to help.",
